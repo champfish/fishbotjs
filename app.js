4a440d00b4570eb6ae244bcd20cbf886128a0708
CJ Duffee <champfish@users.noreply.github.com>
2017-12-01 10:37:15 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = '-ZgN7xVZlpHDiY-TYpcCLrL0YVgrsdwv'
 
+console.log('Bot Started!');
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
